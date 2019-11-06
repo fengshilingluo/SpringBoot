3da233f9633db1d18d21142f8a4f12184d471be4
fengshilingluo <57441051+fengshilingluo@users.noreply.github.com>
2019-11-06 20:44:04 +0800

    第一次测试

--- src/main/java/com/baizhi/action/UserAction.java
+++ src/main/java/com/baizhi/action/UserAction.java
@@ -3,7 +3,7 @@ package com.baizhi.action;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+//GITHUB上的修改
 @Controller
 @RequestMapping("/user")
 public class UserAction {
